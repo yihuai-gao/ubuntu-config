2deb0e3aa3782ec9a16a835653805b9708a9063e
David Gao <yhgao@mit.edu>
2022-10-14 14:38:00 +0800

    Reorder

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -9,11 +9,7 @@ sudo apt update && sudo apt install zsh --yes
 sudo apt install curl git python3-pip python vim net-tools
 sudo apt install ca-certificates --reinstall
 pip3 install wheel ranger-fm archey4
-sudo apt-get install fzf ||{
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
-}
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+./install_oh_my_zsh.sh
 sudo apt-get install python-pip
 git config --global user.name "DavidGao"
 git config --global user.email "davidgao1013@gmail.com"
@@ -21,3 +17,8 @@ touch ~/.gitignore_global
 echo ".DS_Store
 .vscode" >> ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+
+sudo apt-get install fzf ||{
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
