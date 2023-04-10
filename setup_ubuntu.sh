6a2ab4f5befd90925564535d16b72ea9e8e6aadd
DavidGao <davidgao1013@gmail.com>
2023-04-10 21:13:01 +0800

    remove sshkey and switchey omega

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,9 +1,3 @@
-# ssh-keygen -t ed25519
-touch ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIK7guJTbrJLZuyeJ7oNDVGmE+NU92J/Uu2I9uNj96Lz davidgao1013@gmail.com" >> ~/.ssh/authorized_keys
-# echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-
 sudo apt-get update
 sudo apt update && sudo apt install zsh --yes
 sudo apt install curl git python3-pip python vim net-tools
