2524a7d7d944e0932d465457c5887be993729f04
DavidGao <davidgao1013@gmail.com>
2023-10-06 11:08:26 -0700

    Update filename

--- install_conda.sh
+++ install_conda.sh
@@ -1,5 +1,5 @@
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 bash Miniforge3-Linux-x86_64.sh -b -p ~/miniforge3
 rm Miniforge3-Linux-x86_64.sh
-~/miniforge/bin/conda init zsh
-~/miniforge/bin/conda init bash
+~/miniforge3/bin/conda init zsh
+~/miniforge3/bin/conda init bash
