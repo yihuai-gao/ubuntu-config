4a0c91e89bc9c019216eb5194c12f131293d0754
Yihuai Gao <yihuai@stanford.edu>
2024-12-28 22:57:09 -0800

    Update fonts

--- install_fonts.sh
+++ install_fonts.sh
@@ -2,5 +2,6 @@
 # You can update the GNOME terminal font in Preference -> Profiles (usually Unnamed) -> Costom font
 # If you are using VSCode remote-ssh to connect to your linux server, you can search "terminal font" in vscode preferences
 # and set it to "SauceCodePro Nerd Font"
-sudo cp 'Sauce Code Pro Nerd Font Complete.ttf' /usr/local/share/fonts
+mkdir -p $HOME/.local/share/fonts
+cp -r SourceCodePro $HOME/.local/share/fonts
 fc-cache -f -v
