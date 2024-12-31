494ccd6c5339edf08c0590b5ff05c4982d67c661
Yihuai Gao <yihuai@stanford.edu>
2024-12-30 19:29:32 -0800

    Fix config bugs

--- config_cursor.sh
+++ config_cursor.sh
@@ -2,16 +2,17 @@
 
 action=$1
 
-if [ "$action" = "get" ]; then
-    # get this file directory even if not cd'd to this directory
-    file_dir=$(dirname $(realpath $0))
+# get this file directory even if not cd'd to this directory
+file_dir=$(dirname $(realpath $0))
+
+if [ "$action" = "pull" ]; then
 
     # get the current cursor config
     cp ~/.config/Cursor/User/keybindings.json $file_dir/configs/cursor/keybindings.json
     cp ~/.config/Cursor/User/settings.json $file_dir/configs/cursor/settings.json
 fi
 
-if [ "$action" = "set" ]; then
+if [ "$action" = "push" ]; then
     # set the cursor config
     cp $file_dir/configs/cursor/keybindings.json ~/.config/Cursor/User/keybindings.json
     cp $file_dir/configs/cursor/settings.json ~/.config/Cursor/User/settings.json
