bcb8687bdf1c30df85557d5949e7a38ddb2075d3
Yihuai Gao <yihuai@stanford.edu>
2025-01-19 17:28:39 -0800

    Create paths if not exist when installing user apps

--- install_user_apps.sh
+++ install_user_apps.sh
@@ -7,7 +7,11 @@ mkdir -p $HOME/.local
 mkdir -p $file_dir/downloads
 cd $file_dir/downloads
 
-apt download fzf zoxide ncdu neofetch
+apt download fzf
+apt download zoxide
+apt download ncdu
+apt download neofetch
+
 dpkg-deb -x fzf* $HOME/.local
 dpkg-deb -x zoxide* $HOME/.local
 dpkg-deb -x ncdu* $HOME/.local
@@ -17,5 +21,7 @@ cd $file_dir
 
 $HOME/miniforge3/bin/mamba install -y yazi
 $HOME/miniforge3/bin/mamba install -y nvitop
+
+mkdir -p $HOME/.local/usr/bin
 ln -s $HOME/miniforge3/bin/yazi $HOME/.local/usr/bin/yazi
 ln -s $HOME/miniforge3/bin/nvitop $HOME/.local/usr/bin/nvitop
