cd8d31c157a1d4f93c815510edca01995acf0693
Yihuai Gao <yihuai@stanford.edu>
2025-07-14 15:04:16 -0700

    Enable creating vscode socket

--- slurm/aws_iad.sh
+++ slurm/aws_iad.sh
@@ -3,6 +3,22 @@ file_dir=$(dirname $(realpath $0))
 alias sdbg4="sbatch $file_dir/jupyter_4hrs.sbatch"
 alias sdbg48="sbatch $file_dir/jupyter_48hrs.sbatch"
 
-alias sqe="squeue -u $USER --format='%.18i %.9P %.30j %.8T %.10M %.6D %R'"
-alias sq="squeue"
+alias sqe="squeue -u $USER --format='%.10i %.20P %.30j %.8T %.10M %.6D %R'"
+alias sq="squeue  --format='%.10i %.20P %.30j %.8T %.10M %.6D %R'"
 alias sc="scancel --signal=KILL"
+
+jupyter_addr() {
+    # Find the 3 largest number
+        find /project/cosmos/yihuaig/jupyter_jobs -maxdepth 1 -type d -printf "%f\n" \
+        | grep -E '^[0-9]+$' | sort -n | tail -n 3 | while read -r num; do
+            file="/project/cosmos/yihuaig/jupyter_jobs/$num/jupyter.txt"
+            if [ -f "$file" ]; then
+                head -n 1 "$file"
+            else
+                echo "No jupyter.txt found for $num"
+            fi
+    done
+}
+
+
+alias start_tunneling="$file_dir/start_tunneling.sh"
--- slurm/start_tunneling.sh
+++ slurm/start_tunneling.sh
@@ -1,5 +1,7 @@
 # Debug in vscode using tunnel in the terminal of interactive node
 
+mkdir -p /run/user/$HOST_USER_ID # Create the run directory so that cursor socket can be created
+
 # Run in the enroot environment
 wget  https://api2.cursor.sh/updates/download-latest?os=cli-alpine-x64 -O /tmp/cursor.tar.gz;
 
