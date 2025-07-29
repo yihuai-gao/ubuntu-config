a10634c24ace74639bbda699bb865e150cdb95d6
Yihuai Gao <yihuai@stanford.edu>
2025-07-29 13:33:58 -0700

    Update phrases for sogoupinyin

---
+++ config_sogoupinyin.sh
@@ -0,0 +1,4 @@
+file_dir=$(dirname $(realpath $0))
+
+cp $file_dir/configs/sogoupinyin/Phrases.ini ~/.config/sogoupinyin/dict/PCWBDict/Phrases.ini
+cp $file_dir/configs/sogoupinyin/Phrases.ini ~/.config/sogoupinyin/dict/PCPYDict/phrase/PhraseEdit.txt
