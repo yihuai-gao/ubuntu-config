df958c90e0a13a984e135be958cae9c9166df4fb
Yihuai Gao <yihuai@stanford.edu>
2025-11-28 11:19:46 -0800

    Manually install fzf

--- install_user_apps.sh
+++ install_user_apps.sh
@@ -7,22 +7,18 @@ mkdir -p $HOME/.local
 mkdir -p $file_dir/downloads
 cd $file_dir/downloads
 
-apt download fzf
 apt download zoxide
 apt download neofetch
-apt download sysstat
-apt download fio
 apt download x11-apps
 wget https://dev.yorhel.nl/download/ncdu-2.8.1-linux-x86_64.tar.gz
+wget https://github.com/junegunn/fzf/releases/download/v0.67.0/fzf-0.67.0-linux_amd64.tar.gz
 
 mkdir -p $HOME/.local/usr/bin
 tar -xzf ncdu-2.8.1-linux-x86_64.tar.gz -C $HOME/.local/usr/bin
+tar -xzf fzf-0.67.0-linux_amd64.tar.gz -C $HOME/.local/usr/bin
 
-dpkg-deb -x fzf* $HOME/.local
 dpkg-deb -x zoxide* $HOME/.local
 dpkg-deb -x neofetch* $HOME/.local
-dpkg-deb -x sysstat* $HOME/.local
-dpkg-deb -x fio* $HOME/.local
 dpkg-deb -x x11-apps* $HOME/.local
 cd $file_dir
 
--- slurm/aws_iad.sh
+++ slurm/aws_iad.sh
@@ -3,8 +3,12 @@ file_dir=$(dirname $(realpath $0))
 alias sdbg4="sbatch $file_dir/jupyter_4hrs.sbatch"
 alias sdbg="sbatch $file_dir/jupyter_48hrs.sbatch"
 
-alias sqe="squeue -u $USER --format='%.15i %.20P %.70j %.8T %.10M %.6D %R'"
-alias sq="squeue  --format='%.15i %.20P %.100j %.10u %.8T %.10M %.6D %R'"
+alias sqe="squeue -u $USER --format='%.15i %.20P %.60j %.8T %.10M %.6D %R' | grep -v 'train_job_killable'"
+alias sqc="squeue --format='%.15i %.20P %.60j %.8T %.10u %.10M %.6D %R' | grep COMP"
+alias sqp="squeue --format='%.15i %.20P %.60j %.8T %.10u %.10M %.6D %R' | grep PEND"
+alias sqd="squeue --format='%.15i %.20P %.60j %.8T %.10u %.10M %.6D %R' | grep debug"
+
+alias sq="squeue  --format='%.15i %.20P %.60j %.10u %.8T %.10M %.6D %R'"
 alias sc="scancel --signal=KILL"
 alias srq="scontrol requeue"
 
@@ -25,15 +29,15 @@ jupyter_addr() {
 alias start_tunneling="$file_dir/start_tunneling.sh"
 
 # For imitation_learning_policies
-alias sim_train="sbatch slurm_scripts/slurm_training_sim.sbatch"
-alias real_train="sbatch slurm_scripts/slurm_training_real.sbatch"
-alias seval="sbatch slurm_scripts/slurm_eval.sbatch"
+alias ssim="sbatch slurm_scripts/ssim.sh"
+alias sreal="sbatch slurm_scripts/sreal.sh"
+alias seval="sbatch slurm_scripts/seval.sh"
 
 init_container() {
 
     # Install system dependencies
     apt update
-    apt install libnl-genl-3-200 -y
+    apt install libnl-genl-3-200 -y # For htop
     apt install lsof -y
     apt install rclone fuse3 -y
     apt install lz4 -y
